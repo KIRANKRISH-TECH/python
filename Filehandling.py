844adc419d2514d8823b01acbfd3cc1ad5083110
KIRANKRISH-TECH <krishkiran098@gmail.com>
2025-08-06 11:00:34 +0530

    Add files via upload

---
+++ Filehandling.py
@@ -0,0 +1,53 @@
+# with open("hello.txt", "r") as f:
+#     f.write("woops! i have made a mistake")
+
+
+
+
+# write csv file 
+
+
+# import csv 
+# with open("protogonist.csv", "w", newline='') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["Name", "Age", "Occupation"])
+#     writer.writerow(["Alice", 30, "Engineer"])
+#     writer.writerow(["Bob", 25, "Designer"])
+#     writer.writerow(["Charlie", 35, "Teacher"])
+
+
+
+
+
+# # read CSV file
+
+
+
+# with open ("protogonist.csv", "r") as f:
+#     reader= csv.reader(f)
+#     for row in reader:
+#         print(row)
+
+
+
+# zip
+
+# import zipfile
+# with zipfile.zipfile("comp.zip", "r") as f:
+#     f.write("excel.csv")         
+#     f.write("fun.txt")
+
+
+
+
+
+
+
+
+# read
+
+# f=open("hello.txt", "r")
+# print(f.read(14))
+
+
+
---
+++ acces.py
@@ -0,0 +1,46 @@
+
+
+            #    protected 
+            
+# class bankaccount:
+
+#     def __init__(self, balance):
+#         self.__balance = balance
+
+#     def __calculate_interest(self):
+#         return self.__balance * 0.05  # 5% interest
+
+#     def __show_balance(self):
+#         print(f"Account balance: ${self.__balance}")
+#         print(f"Interest earned: ${self.__calculate_interest()}")
+
+# account = bankaccount(1000)
+# account._bankaccount__show_balance()
+
+
+
+
+# class stock:
+#     def __init__(self, name, price,):
+#         self._name = name
+#         self._price = price
+       
+#     def _stockinfo(self):
+#         print(f"Stock Name: {self._name}")
+#         print(f"Stock Price: ${self._price}")
+#         print(f"stockinfo: {self._name} is trading at ${self._price}")
+# stock1 = stock("bitcoin", 1000000)
+# stock1._stockinfo()    
+# stock2=stock("ethereum", 2000000)
+# stock2._stockinfo()
+# stock3=stock("infy", 3000000)
+# stock3._stockinfo()
+# stock4=stock("dogecoin", 4000000)
+# stock4._stockinfo() 
+           
+
+
+
+
+
+
