6de7190aba24c07495f5ff10283ff4349b5aa74c
kiran.A <krishkiran098@gmail.com>
2025-08-06 12:20:37 +0530

    Add files via upload

--- assigment/modules.py
+++ assigment/modules.py
@@ -2,4 +2,22 @@
 #     print("hello",+name)
 
 
+#................... calendar................
+
+
+# import calendar
+# yy=2025
+# mm=8
+# print(calendar.month(yy, mm))
+
+
+# ......................# random........................ 
+# import random
+# random.seed(10)
+# print(random.random())
+
+
+# import random
+# random.getstate()
+# print(random.random())
 
